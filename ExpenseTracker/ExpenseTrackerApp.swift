e8b1ac738b8eb42333f3964a6c8a0586512a5480
rgsk <guptarahul70322@gmail.com>
2022-10-26 18:21:19 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Rahul Gupta on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
